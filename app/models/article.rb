84d4aa623d958c42f725fbd0d48a7729fec657c1
andrisyam <andri.syamsuar@midtrans.com>
2018-05-25 14:46:11 +0700

    commit article edit update & destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,3 @@
 class Article < ActiveRecord::Base
-  validate :title, presence: true, length: { minimum:3, maximum: 50 }
-  validate :description, presence: true, length: { minimum:10, maximum: 300 }
+  validates_presence_of :title, :description
 end
